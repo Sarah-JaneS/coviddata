d960a47b9c55a5b0119c4485cd3511455f4e9a0f
Sarah-JaneS <sarahjaneseddon@gmail.com>
2021-10-18 21:49:45 +1300

    Got p5 js installed

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,17 @@
+// put setup code here
+function setup() {
+  createCanvas(400, 400);
+}
+
+
+
+// put drawing code here
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
+  
