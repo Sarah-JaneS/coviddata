a2c34932c33ea2b431e5288c42d6c3d8e3c8b6a3
Sarah-JaneS <sarahjaneseddon@gmail.com>
2021-10-18 22:45:35 +1300

    Working out how to use p5.js

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,17 +1,39 @@
-// put setup code here
-function setup() {
-  createCanvas(400, 400);
-}
+let table;
+
+function setup (){
+  createCanvas(1000,1000);
+  background(0);
+  nofill();
+  table = loadTable ('covidnz.csv', 'header');
+  for (let r=0;r < table.getRowCount(); r++){
+    stroke(0, random(255),0)
+    ellipse (width/2, covidnz.getInt(i, "TOTAL"), covidnz.getInt(r, "TOTAL"));
+    if (covidnz.getInt(i, "TOTAL") ==22){
+      strokeWeight(8);
+  }
+ // for (let c= 0; c < table.getColumnCount(); c++ {
+    //print(table.getString(r,c));
+  }
+  
+
+
+
+
+ // for(int i = 0; ri < covidnz.getRowCount(); i++){
+  //  stroke(0, random(255),0)
+    //strokeWeight(3);
+    //ellipse (width/2, covidnz.getInt(i, "TOTAL"), covidnz.getInt(i, "TOTAL"));
+    //  if (covidnz.getInt(i, "TOTAL") ==22){
+     //   strokeWeight(8);
+     // }
+  //}
+
 
 
 
 // put drawing code here
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+
 }
-  
+}
+
