f043de99512a2d4963524e240b548a6157a857c0
Sarah-JaneS <sarahjaneseddon@gmail.com>
2021-10-18 23:20:05 +1300

    Getting data in

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,16 +1,37 @@
 let table;
 
+
+
+function preload() {
+  table = loadTable ('covidnz.csv', 'csv', 'header');
+  
+}
+
 function setup (){
-  createCanvas(1000,1000);
-  background(0);
-  nofill();
-  table = loadTable ('covidnz.csv', 'header');
+
+  createCanvas(800, 400);
+  background(220);
+  textAlign(CENTER, TOP);
   for (let r=0;r < table.getRowCount(); r++){
-    stroke(0, random(255),0)
-    ellipse (width/2, covidnz.getInt(i, "TOTAL"), covidnz.getInt(r, "TOTAL"));
-    if (covidnz.getInt(i, "TOTAL") ==22){
-      strokeWeight(8);
+    const name = table.getString (r, "DHB");
+    const cases = table.getNum (r, "TOTAL");
+    const x = random(0, width);
+    const y = random (0, height);
+    const size = map(cases, 0, 293, 0, 200 )
+    ellipse (x, y, size)
+    text(name, x , y, size / 2)
+
   }
+
+  //console.log(table);
+
+  ///table = loadTable ('covidnz.csv', 'header');
+  //for (let r=0;r < table.getRowCount(); r++){
+   // stroke(0, random(255),0)
+  //  ellipse (width/2, covidnz.getInt(i, "TOTAL"), covidnz.getInt(r, "TOTAL"));
+   // if (covidnz.getInt(i, "TOTAL") ==22){
+  //    strokeWeight(8);z
+
  // for (let c= 0; c < table.getColumnCount(); c++ {
     //print(table.getString(r,c));
   }
@@ -33,7 +54,8 @@ function setup (){
 
 // put drawing code here
 function draw() {
+  rect(10, 20, 30, 40);
 
 }
-}
+
 
